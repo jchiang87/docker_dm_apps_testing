fbc5b4c8e2b2e5072e6ad5a8e252ece75d211df5
Jim Chiang <jchiang@slac.stanford.edu>
2017-12-17 22:28:34 -0800

    pip install coveralls in travis_test.sh

--- travis_test.sh
+++ travis_test.sh
@@ -2,6 +2,7 @@
 source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 pip install nose
+pip install coveralls
 eups declare apps_test -r /home/vagrant/apps_test -t current
 setup apps_test
 cd /home/vagrant/apps_test
