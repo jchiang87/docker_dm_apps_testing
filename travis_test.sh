411137b84fde9b512373db936d7b807a315b2b8c
Jim Chiang <jchiang@slac.stanford.edu>
2017-12-17 20:56:24 -0800

    add :z option

--- travis_test.sh
+++ travis_test.sh
@@ -4,10 +4,6 @@ setup lsst_distrib
 pip install nose
 eups declare apps_test -r /home/vagrant/apps_test -t current
 setup apps_test
-ls
 cd /home/vagrant/apps_test
-ls
-whoami
 nosetests
-setenforce 0
 touch foo
