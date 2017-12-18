5e506b5e8e078fce7eb36729fd15deb7e69ec756
Jim Chiang <jchiang@slac.stanford.edu>
2017-12-18 13:13:17 -0800

    change working dir in container so that coveralls can find source

--- travis_test.sh
+++ travis_test.sh
@@ -3,12 +3,8 @@ source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 pip install nose
 pip install coveralls
-eups declare apps_test -r /home/vagrant/apps_test -t current
+eups declare apps_test -r /home/travis/apps_test -t current
 setup apps_test
-cd /home/vagrant
-groups vagrant
-id vagrant
-ls -l
-cd /home/vagrant/apps_test
+cd /home/travis/apps_test
 ls -l
 nosetests -s --with-coverage --cover-package=desc.apps_test
